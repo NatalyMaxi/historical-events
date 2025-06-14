dde82b54e4bf921d70554684898649ccd9a0bf6a
Natalya Maximova <natka-31@mail.ru>
2025-06-14 11:33:37 +0500

    feat: add fonts, icons, basic styles, mixins

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,8 +3,18 @@ module.exports = {
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
     'order/properties-alphabetical-order': null,
+    'at-rule-no-unknown': null,
+    'custom-property-empty-line-before': null,
     'scss/at-rule-no-unknown': true,
     'scss/dollar-variable-pattern': null,
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+        ignoreAtRules: ['else'],
+      },
+    ],
   },
   ignoreFiles: ['node_modules/**', 'build/**', 'dist/**'],
 };
